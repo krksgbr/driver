3e18bbbd2cbc292680243430dd34eefe4676387f
pukkamustard <adarsh@dividat.com>
2017-12-05 23:17:30 +0100

    Use context when dialing tcp connection

--- src/senso/tcp.go
+++ src/senso/tcp.go
@@ -14,9 +14,6 @@ import (
 // How long to wait before timeing out a tcp connection attempt
 const dialTimeout = 5 * time.Second
 
-// never stop retrying to connect
-const maxElapsedTime = 0
-
 // maximal interval to wait between connection retry
 const maxInterval = 30 * time.Second
 
@@ -38,87 +35,77 @@ func connectTCP(ctx context.Context, baseLogger *logrus.Entry, address string, d
 		log.Info("Dialing TCP connection.")
 		conn, connErr = dialer.DialContext(ctx, "tcp", address)
 
-		if connErr, ok := connErr.(net.Error); ok && !connErr.Temporary() {
-			return nil
-		}
-
 		if connErr != nil {
 			log.WithError(connErr).Info("Could not connect with Senso.")
 		}
 		return connErr
 	}
 
-	var backOffStrategy = backoff.NewExponentialBackOff()
-
+	// Exponential backoff
+	var expBackoff = backoff.NewExponentialBackOff()
 	// Never stop retrying
-	backOffStrategy.MaxElapsedTime = maxElapsedTime
-
+	expBackoff.MaxElapsedTime = 0
 	// Set maximum interval to 30s
-	backOffStrategy.MaxInterval = maxInterval
+	expBackoff.MaxInterval = maxInterval
+
+	var backOffStrategy = backoff.WithContext(expBackoff, ctx)
 
 	for true {
 
 		backOffStrategy.Reset()
 		backoff.Retry(dialTCP, backOffStrategy)
 
-		if conn != nil {
-
-			log.Info("Connected.")
-
-			// Close connection if we break or return
-			defer conn.Close()
-
-			// create channel for reading data and go read
-			readChannel := make(chan []byte)
-			go tcpReader(log, conn, readChannel)
-
-			// create channel for writing data
-			writeChannel := make(chan []byte)
-			// We need an additional channel for handling write errors, unlike the readChannel we don't want to close the channel as somebody might try to write to it
-			writeErrors := make(chan error)
-			defer close(writeChannel)
-			go tcpWriter(conn, writeChannel, writeErrors)
-
-			// Inner loop for handling data
-			disconnected := false
-			for !disconnected {
-				select {
-
-				case <-ctx.Done():
-					return
-
-				case receivedData, more := <-readChannel:
-					if more {
-						// Attempt to send data, if can not send immediately discard
-						select {
-						case data <- receivedData:
-						default:
-						}
-					} else {
-						disconnected = true
-					}
-
-				case dataToWrite := <-data:
-					writeChannel <- dataToWrite
-
-				case writeError := <-writeErrors:
-					if err, ok := writeError.(net.Error); ok && err.Timeout() {
-						log.Debug("Timeout while writing.")
-					} else {
-						log.WithError(writeError).Error("Write error.")
-						disconnected = true
-					}
-				}
-			}
+		// connection/ctx has been cancelled
+		if conn == nil {
+			return
 		}
 
-		// Check if connection has been cancelled
-		select {
-		case <-ctx.Done():
-			return
+		log.Info("Connected.")
 
-		default:
-			log.Debug("Reconnecting.")
+		// Close connection if we break or return
+		defer conn.Close()
+
+		// create channel for reading data and go read
+		readChannel := make(chan []byte)
+		go tcpReader(log, conn, readChannel)
+
+		// create channel for writing data
+		writeChannel := make(chan []byte)
+		// We need an additional channel for handling write errors, unlike the readChannel we don't want to close the channel as somebody might try to write to it
+		writeErrors := make(chan error)
+		defer close(writeChannel)
+		go tcpWriter(conn, writeChannel, writeErrors)
+
+		// Inner loop for handling data
+		disconnected := false
+		for !disconnected {
+			select {
+
+			case <-ctx.Done():
+				return
+
+			case receivedData, more := <-readChannel:
+				if more {
+					// Attempt to send data, if can not send immediately discard
+					select {
+					case data <- receivedData:
+					default:
+					}
+				} else {
+					disconnected = true
+				}
+
+			case dataToWrite := <-data:
+				writeChannel <- dataToWrite
+
+			case writeError := <-writeErrors:
+				if err, ok := writeError.(net.Error); ok && err.Timeout() {
+					log.Debug("Timeout while writing.")
+				} else {
+					log.WithError(writeError).Error("Write error.")
+					disconnected = true
+				}
+			}
 		}
 
 	}
@@ -133,13 +120,12 @@ func tcpReader(log *logrus.Entry, conn net.Conn, channel chan<- []byte) {
 
 	// Loop and read from connection.
 	for {
-		// conn.SetReadDeadline(time.Now().Add(100 * time.Millisecond))
 		readN, readErr := conn.Read(buffer)
 
 		if readErr != nil {
 			if readErr == io.EOF {
 				// connection is closed
-				log.Info("Connection closed (EOF).")
+				log.Info("Connection closed by Senso.")
 				return
 			} else if err, ok := readErr.(net.Error); ok && err.Timeout() {
 				// Read timeout, just continue Nothing
