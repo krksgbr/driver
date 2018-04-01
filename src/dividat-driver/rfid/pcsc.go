ecabf40bba8570d4a6e92e2b777d0a7ba3dc97fa
Johannes Emerich <johannes@emerich.de>
2018-04-01 11:46:54 +0200

    Use extended waiting time for status change
    Use cancellation of status change calls from above to allow running
    `GetStatusChange` in a separate goroutine and use extended waiting
    periods.

--- src/dividat-driver/rfid/pcsc.go
+++ src/dividat-driver/rfid/pcsc.go
@@ -9,8 +9,8 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-var READER_POLLING_INTERVAL = 500 * time.Millisecond
-var CARD_POLLING_INTERVAL = 50 * time.Millisecond
+var READER_POLLING_INTERVAL = 1 * time.Second
+var CARD_POLLING_TIMEOUT = 1 * time.Second
 
 // Special MSFT name to bolt plug&play onto PC/SC
 // Supported by winscard and libpcsc
@@ -37,107 +37,112 @@ func pollSmartCard(ctx context.Context, log *logrus.Entry, callback func(string)
 
 	log.WithField("pnp", hasPnP).Info("Starting RFID scanner.")
 
-	var availableReaders []string = make([]string, 0)
-	var lastKnownState map[string]scard.StateFlag = map[string]scard.StateFlag{}
+	go waitForCardActivity(log, scard_ctx, hasPnP, callback)
+
+	<-ctx.Done()
+	// Cancel `GetStatusChange`
+	scard_ctx.Cancel()
+
+	log.Info("Stopping RFID scanner.")
+}
+
+func waitForCardActivity(log *logrus.Entry, scard_ctx *scard.Context, hasPnP bool, callback func(string)) {
+	availableReaders := make([]string, 0)
+	lastKnownState := map[string]scard.StateFlag{}
 
 	for {
-
-		select {
-		case <-ctx.Done():
-			log.Info("Stopping RFID scanner.")
+		// Retrieve available readers
+		newReaders, err := scard_ctx.ListReaders()
+		if err != nil {
+			log.WithError(err).Error("Error listing readers.")
 			return
+		}
+		announceReaderChanges(log, availableReaders, newReaders)
+		availableReaders = newReaders
 
-		default:
-			// Retrieve available readers
-			newReaders, err := scard_ctx.ListReaders()
+		// Wait for readers to appear
+		if len(availableReaders) == 0 {
+			if hasPnP {
+				// `GetStatusChange` acts as a smarter sleep that finishes early
+				code := scard_ctx.GetStatusChange(
+					[]scard.ReaderState{makeReaderState(MAGIC_PNP_NAME)},
+					READER_POLLING_INTERVAL,
+				)
+				if code == scard.ErrCancelled {
+					return
+				}
+			} else {
+				time.Sleep(READER_POLLING_INTERVAL)
+			}
+
+			// Restart loop to list readers
+			continue
+		}
+
+		// Now there are available readers
+		// Wait for card presence
+		readerStates := make([]scard.ReaderState, len(availableReaders))
+		for ix, readerName := range availableReaders {
+			flag, ok := lastKnownState[readerName]
+			if ok {
+				readerStates[ix] = makeReaderState(readerName, flag)
+			} else {
+				readerStates[ix] = makeReaderState(readerName)
+			}
+		}
+		// We need to timeout perodically to check for new readers
+		code := scard_ctx.GetStatusChange(readerStates, CARD_POLLING_TIMEOUT)
+		if code == scard.ErrCancelled {
+			return
+		} else if code != nil {
+			continue
+		}
+
+		// One or more readers changed their status
+		for _, readerState := range readerStates {
+
+			if readerState.CurrentState == readerState.EventState {
+				// This reader has not changed.
+				continue
+			}
+
+			if is(readerState.EventState, scard.StateChanged) {
+				// Event state becomes current state
+				readerState.CurrentState = readerState.EventState
+				// Keep track of last known state for next refresh cycle
+				lastKnownState[readerState.Reader] = readerState.CurrentState
+			} else {
+				continue
+			}
+
+			if !is(readerState.CurrentState, scard.StatePresent) {
+				// This reader has no card.
+				continue
+			}
+
+			// Connect to the card
+			card, err := scard_ctx.Connect(readerState.Reader, scard.ShareShared, scard.ProtocolAny)
 			if err != nil {
-				log.WithError(err).Error("Error listing readers.")
-				return
-			}
-			announceReaderChanges(log, availableReaders, newReaders)
-			availableReaders = newReaders
-
-			// Wait for readers to appear
-			if len(availableReaders) == 0 {
-				if hasPnP {
-					// `GetStatusChange` acts as a smarter sleep that finishes early
-					pnpReaderStates := []scard.ReaderState{
-						makeReaderState(MAGIC_PNP_NAME),
-					}
-					scard_ctx.GetStatusChange(pnpReaderStates, READER_POLLING_INTERVAL)
-				} else {
-					time.Sleep(READER_POLLING_INTERVAL)
-				}
-
-				// Restart loop to list readers
+				log.WithError(err).Error("Error connecting to card.")
 				continue
 			}
 
-			// Now there are available readers
-			// Wait for card presence
-			readerStates := make([]scard.ReaderState, len(availableReaders))
-			for ix, readerName := range availableReaders {
-				flag, ok := lastKnownState[readerName]
-				if ok {
-					readerStates[ix] = makeReaderState(readerName, flag)
-				} else {
-					readerStates[ix] = makeReaderState(readerName)
-				}
-			}
-			// We could block until a card status changes here, but have to
-			// unblock periodically to check if our context has been cancelled
-			code := scard_ctx.GetStatusChange(readerStates, CARD_POLLING_INTERVAL)
-			if code == scard.ErrTimeout {
+			// Request UID
+			response, err := card.Transmit(UID_APDU)
+			if err != nil {
+				log.WithError(err).Debug("Failed while transmitting APDU.")
 				continue
 			}
-
-			// One or more readers changed their status
-			for _, readerState := range readerStates {
-				fmt.Println("LOOPING readers")
-
-				if readerState.CurrentState == readerState.EventState {
-					// This reader has not changed.
-					continue
-				}
-
-				if is(readerState.EventState, scard.StateChanged) {
-					// Event state becomes current state
-					readerState.CurrentState = readerState.EventState
-					// Keep track of last known state for next refresh cycle
-					lastKnownState[readerState.Reader] = readerState.CurrentState
-				} else {
-					continue
-				}
-
-				if !is(readerState.CurrentState, scard.StatePresent) {
-					// This reader has no card.
-					continue
-				}
-
-				// Connect to the card
-				card, err := scard_ctx.Connect(readerState.Reader, scard.ShareShared, scard.ProtocolAny)
-				if err != nil {
-					log.WithError(err).Error("Error connecting to card.")
-					continue
-				}
-
-				// Request UID
-				response, err := card.Transmit(UID_APDU)
-				if err != nil {
-					log.WithError(err).Debug("Failed while transmitting APDU.")
-					continue
-				}
-				uid := ""
-				for i := 0; i < len(response)-2; i++ {
-					uid += fmt.Sprintf("%X", response[i])
-				}
-				if len(uid) > 0 {
-					log.Info("Detected RFID token.")
-					callback(uid)
-				}
-
-				card.Disconnect(scard.UnpowerCard)
+			uid := ""
+			for i := 0; i < len(response)-2; i++ {
+				uid += fmt.Sprintf("%X", response[i])
 			}
+			if len(uid) > 0 {
+				log.Info("Detected RFID token.")
+				callback(uid)
+			}
+
+			card.Disconnect(scard.UnpowerCard)
 		}
 	}
 }
