10f89afaa57939c2dabd9b8516ef5f894691f3bc
pukkamustard <pukkamustard@posteo.net>
2018-04-18 11:40:47 +0200

    Update behaviour of replayer to Senso firmware 2.0.0.0

--- tools/replay/control.js
+++ tools/replay/control.js
@@ -1,13 +1,8 @@
-// Moch up the Senso control server.
-
-var net = require('net')
-
-var HOST = '0.0.0.0'
-var PORT = 55567
+// Mock Senso responses
 
 // Firmware version to report in DEV_INFO
-const VERSION_MAJOR = 1
-const VERSION_MINOR = 2
+const VERSION_MAJOR = 2
+const VERSION_MINOR = 0
 const VERSION_FEATURE = 0
 const VERSION_FIX = 0
 
@@ -15,50 +10,47 @@ const VERSION_FIX = 0
 const DATA_TYPE_GET_DEV_INFO = 0xD1
 const DATA_TYPE_GET_VCC_INFO = 0xD2
 
-// Create a server instance, and chain the listen function to it
-// The function passed to net.createServer() becomes the event handler for the 'connection' event
-// The sock object the callback function receives UNIQUE for each connection
-module.exports = net.createServer(function (sock) {
-    // We have a connection - a socket object is assigned to the connection automatically
-  console.log('CONTROL - Connection: ' + sock.remoteAddress + ':' + sock.remotePort)
+module.exports = function (profile, data) {
+  console.log(data)
+  const body = data.slice(8)
+  const type = body.readUInt16LE(2)
+  switch (type) {
+    case DATA_TYPE_GET_DEV_INFO:
+      return devInfo(profile)
 
-    // Add a 'data' event handler to this instance of socket
-  sock.on('data', function (data) {
-    console.log(data)
-    const body = data.slice(8)
-    const type = body.readUInt16LE(2)
-    switch (type) {
-      case DATA_TYPE_GET_DEV_INFO:
-        sock.write(devInfo())
-        break
+    case DATA_TYPE_GET_VCC_INFO:
+      return vccInfo()
 
-      case DATA_TYPE_GET_VCC_INFO:
-        sock.write(vccInfo())
-        break
+    default:
+      return standardResponse(type)
+  }
+}
 
-      default:
-        sock.write(standardResponse(type))
-    }
-  })
-
-    // Add a 'close' event handler to this instance of socket
-  sock.on('close', function (data) {
-    console.log('CONTROL - Closed: ' + sock.remoteAddress + ' ' + sock.remotePort)
-  })
-}).listen(PORT, HOST)
-
-function devInfoItem () {
+function devInfoItem (serial_number) {
   var devInfoItem = Buffer.alloc(32)
+
+  // status code
   devInfoItem.writeUInt32LE(0, 0)
+
+  // error code
   devInfoItem.writeUInt32LE(0, 4)
+
+  // software version
   devInfoItem.writeInt8(VERSION_MAJOR, 11)
   devInfoItem.writeInt8(VERSION_MINOR, 10)
   devInfoItem.writeInt8(VERSION_FEATURE, 9)
   devInfoItem.writeInt8(VERSION_FIX, 8)
+
+  // hardware version
+  devInfoItem.writeUInt32LE(0, 12)
+
+  // serial number
+  devInfoItem.write(serial_number, 16, 16, 'ascii')
+
   return devInfoItem
 }
 
-function devInfo () {
+function devInfo (profile) {
   var header = Buffer.alloc(8)
 
   var lenType = Buffer.alloc(4)
@@ -67,12 +59,12 @@ function devInfo () {
 
   return Buffer.concat([header,
     lenType,
-    devInfoItem(),
-    devInfoItem(),
-    devInfoItem(),
-    devInfoItem(),
-    devInfoItem(),
-    devInfoItem()
+    devInfoItem(profile.board_serial_numbers.controller),
+    devInfoItem(profile.board_serial_numbers.led_boards.center),
+    devInfoItem(profile.board_serial_numbers.led_boards.up),
+    devInfoItem(profile.board_serial_numbers.led_boards.right),
+    devInfoItem(profile.board_serial_numbers.led_boards.down),
+    devInfoItem(profile.board_serial_numbers.led_boards.left)
   ])
 }
 
@@ -120,5 +112,3 @@ function standardResponse (type) {
 
   return Buffer.concat([header, response])
 }
-
-console.log('CONTROL listening on ' + HOST + ':' + PORT)
