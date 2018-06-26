22dde3061d74ffa50569ad2eb98e565e082bc41a
pukkamustard <pukkamustard@posteo.net>
2018-06-26 08:31:04 +0200

    Fix sha256 of nixpkgs

--- nix/nixpkgs.nix
+++ nix/nixpkgs.nix
@@ -6,5 +6,5 @@ in
   { owner = "NixOS"
   ; repo = "nixpkgs"
   ; rev = "9af0ed346d710afbaa37b44b651199381c37543a"
-  ; sha256 = "0cyhrvcgp8hppsvgycr0a0fiz00gcd24vxcxmv22g6dibdf5377h"; })
+  ; sha256 = "1n37piclgaqp8nsk439rhawqpdchj9fbvx8qbxxrhmh3bh7mwxyh"; })
 
