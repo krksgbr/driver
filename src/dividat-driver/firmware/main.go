aabb4279a0fa683ebfae15601db8a7ab1a5944e0
Gabor <krks.gbr@gmail.com>
2024-04-04 13:13:44 +0200

    Introduce callback for firmware update messages

--- src/dividat-driver/firmware/main.go
+++ src/dividat-driver/firmware/main.go
@@ -42,7 +42,12 @@ func Command(flags []string) {
 		os.Exit(1)
 	}
 
-	err = Update(context.Background(), file, deviceSerial, configuredAddr)
+	onProgress := func(progressMsg string) {
+		fmt.Println(progressMsg)
+	}
+
+	Update(context.Background(), file, deviceSerial, configuredAddr, onProgress)
+
 	if err != nil {
 		fmt.Println(err.Error())
 		fmt.Println()
@@ -51,21 +56,23 @@ func Command(flags []string) {
 	}
 }
 
+type OnProgress func(msg string)
+
 // Firmware update workhorse
-func Update(parentCtx context.Context, image io.Reader, deviceSerial *string, configuredAddr *string) (fail error) {
+func Update(parentCtx context.Context, image io.Reader, deviceSerial *string, configuredAddr *string, onProgress OnProgress) (fail error) {
 	// 1: Find address of a Senso in normal mode
 	var controllerHost string
 	if *configuredAddr != "" {
 		// Use specified controller address
 		controllerHost = *configuredAddr
-		fmt.Printf("Using specified controller address '%s'.\n", controllerHost)
+		onProgress(fmt.Sprintf("Using specified controller address '%s'.", controllerHost))
 	} else {
 		// Discover controller address via mDNS
 		ctx, cancel := context.WithTimeout(parentCtx, 15*time.Second)
-		discoveredAddr, err := discover("_sensoControl._tcp", deviceSerial, ctx)
+		discoveredAddr, err := discover("_sensoControl._tcp", deviceSerial, ctx, onProgress)
 		cancel()
 		if err != nil {
-			fmt.Printf("Error: %s\n", err)
+			onProgress(fmt.Sprintf("Error: %s", err))
 		} else {
 			controllerHost = discoveredAddr
 		}
@@ -73,15 +80,15 @@ func Update(parentCtx context.Context, image io.Reader, deviceSerial *string, co
 
 	// 2: Switch the Senso to bootloader mode
 	if controllerHost != "" {
-		err := sendDfuCommand(controllerHost, controllerPort)
+		err := sendDfuCommand(controllerHost, controllerPort, onProgress)
 		if err != nil {
 			// Log the failure, but continue anyway, as the Senso might have been left in
 			// bootloader mode when a previous update process failed. Not all versions of
 			// the firmware automtically exit from the bootloader mode upon restart.
-			fmt.Printf("Could not send DFU command to Senso at %s: %s\n", controllerHost, err)
+			onProgress(fmt.Sprintf("Could not send DFU command to Senso at %s: %s", controllerHost, err))
 		}
 	} else {
-		fmt.Printf("Could not discover a Senso in regular mode, now trying to detect a Senso already in bootloader mode.\n")
+		onProgress("Could not discover a Senso in regular mode, now trying to detect a Senso already in bootloader mode.")
 	}
 
 	// 3: Find address of Senso in bootloader mode
@@ -90,7 +97,7 @@ func Update(parentCtx context.Context, image io.Reader, deviceSerial *string, co
 		dfuHost = *configuredAddr
 	} else {
 		ctx, cancel := context.WithTimeout(parentCtx, 60*time.Second)
-		discoveredAddr, err := discover("_sensoUpdate._udp", deviceSerial, ctx)
+		discoveredAddr, err := discover("_sensoUpdate._udp", deviceSerial, ctx, onProgress)
 		cancel()
 		if err != nil {
 			// Up to firmware 2.0.0.0 the bootloader advertised itself with the same
@@ -100,15 +107,17 @@ func Update(parentCtx context.Context, image io.Reader, deviceSerial *string, co
 			// We do need to rediscover, as the legacy device may still just have
 			// restarted into the bootloader and obtained a new IP address.
 			ctx, cancel := context.WithTimeout(parentCtx, 60*time.Second)
-			legacyDiscoveredAddr, err := discover("_sensoControl._tcp", deviceSerial, ctx)
+			legacyDiscoveredAddr, err := discover("_sensoControl._tcp", deviceSerial, ctx, onProgress)
 			cancel()
 			if err == nil {
 				dfuHost = legacyDiscoveredAddr
 			} else if controllerHost != "" {
-				fmt.Printf("Could not discover update service, trying to fall back to previous discovery %s.\n", controllerHost)
+				onProgress(fmt.Sprintf("Could not discover update service, trying to fall back to previous discovery %s.", controllerHost))
 				dfuHost = controllerHost
 			} else {
-				fail = fmt.Errorf("Could not find any Senso bootloader to transmit firmware to: %s", err)
+				msg := fmt.Sprintf("Could not find any Senso bootloader to transmit firmware to: %s", err)
+				onProgress(msg)
+				fail = fmt.Errorf(msg)
 				return
 			}
 		} else {
@@ -118,17 +127,17 @@ func Update(parentCtx context.Context, image io.Reader, deviceSerial *string, co
 
 	// 4: Transmit firmware via TFTP
 	time.Sleep(5 * time.Second) // Wait to ensure proper TFTP startup
-	err := putTFTP(dfuHost, tftpPort, image)
+	err := putTFTP(dfuHost, tftpPort, image, onProgress)
 	if err != nil {
 		fail = err
 		return
 	}
 
-	fmt.Println("Success! Firmware transmitted to Senso.")
+	onProgress("Success! Firmware transmitted to Senso.")
 	return
 }
 
-func sendDfuCommand(host string, port string) error {
+func sendDfuCommand(host string, port string, onProgress OnProgress) error {
 	// Header
 	const PROTOCOL_VERSION = 0x00
 	const NUMOFBLOCKS = 0x01
@@ -158,12 +167,12 @@ func sendDfuCommand(host string, port string) error {
 		return fmt.Errorf("Could not send DFU command: %v", err)
 	}
 
-	fmt.Printf("Sent DFU command to %s:%s.\n", host, port)
+	onProgress(fmt.Sprintf("Sent DFU command to %s:%s.", host, port))
 
 	return nil
 }
 
-func putTFTP(host string, port string, image io.Reader) error {
+func putTFTP(host string, port string, image io.Reader, onProgress OnProgress) error {
 	client, err := tftp.NewClient(fmt.Sprintf("%s:%s", host, port))
 	if err != nil {
 		return fmt.Errorf("Could not create tftp client: %v", err)
@@ -176,18 +185,18 @@ func putTFTP(host string, port string, image io.Reader) error {
 	if err != nil {
 		return fmt.Errorf("Could not read from file: %v", err)
 	}
-	fmt.Printf("%d bytes sent\n", n)
+	onProgress(fmt.Sprintf("%d bytes sent", n))
 	return nil
 }
 
-func discover(service string, deviceSerial *string, ctx context.Context) (addr string, err error) {
+func discover(service string, deviceSerial *string, ctx context.Context, onProgress OnProgress) (addr string, err error) {
 	resolver, err := zeroconf.NewResolver(nil)
 	if err != nil {
 		err = fmt.Errorf("Initializing discovery failed: %v", err)
 		return
 	}
 
-	fmt.Printf("Starting discovery: %s\n", service)
+	onProgress(fmt.Sprintf("Starting discovery: %s", service))
 
 	entries := make(chan *zeroconf.ServiceEntry)
 
@@ -220,7 +229,7 @@ func discover(service string, deviceSerial *string, ctx context.Context) (addr s
 		}
 		for _, addrCandidate := range entry.AddrIPv4 {
 			if addrCandidate.String() == "0.0.0.0" {
-				fmt.Printf("Skipping discovered address 0.0.0.0 for %s.\n", serial)
+				onProgress(fmt.Sprintf("Skipping discovered address 0.0.0.0 for %s.", serial))
 			} else {
 				devices[serial] = append(devices[serial], addrCandidate.String())
 			}
@@ -234,7 +243,7 @@ func discover(service string, deviceSerial *string, ctx context.Context) (addr s
 	} else if len(devices) == 1 {
 		for serial, addrs := range devices {
 			addr = addrs[0]
-			fmt.Printf("Discovered %s at %v, using %s.\n", serial, addrs, addr)
+			onProgress(fmt.Sprintf("Discovered %s at %v, using %s.", serial, addrs, addr))
 			return
 		}
 	} else {
