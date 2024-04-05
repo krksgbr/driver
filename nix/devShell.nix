c6f5c2640f3ab384bf8f22c16146960984056bf6
Gábor Kerekes <krks.gbr@gmail.com>
2024-04-05 23:09:25 +0200

    Remove unnecessary shellhook

--- nix/devShell.nix
+++ nix/devShell.nix
@@ -25,11 +25,4 @@ mkShell
 
   ] ++ lib.optional stdenv.isDarwin pkgs.darwin.apple_sdk.frameworks.PCSC # PCSC on Darwin
   ++ lib.optional stdenv.isLinux pcsclite;
-
-  # GOPATH is set to a readonly directory
-  # This seems to be fixed with nixpkgs 20.03
-  # https://github.com/NixOS/nixpkgs/issues/90136
-  shellHook = ''
-    export GOPATH="$HOME/.go"
-  '';
 }
